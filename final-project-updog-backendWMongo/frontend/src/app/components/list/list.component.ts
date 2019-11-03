b2a2a5ce6f992d061de4c086b9a0ec3f0cd856f1
shiyao feng <32398815+Windbless@users.noreply.github.com>
2019-11-02 21:55:38 -0700

    try to create different table and set it on list.component.css

--- final-project-updog-backendWMongo/frontend/src/app/components/list/list.component.ts
+++ final-project-updog-backendWMongo/frontend/src/app/components/list/list.component.ts
@@ -3,12 +3,21 @@ import { RunService } from '../../run.service'
 import { Run } from '../../run.model'
 import { MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router'
-
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
+  /*
+  check it next
+  animations: [ 
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ])
+  ]*/
 })
 export class ListComponent implements OnInit {
 
